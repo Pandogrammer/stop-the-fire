773f8ce14d9120baaecd8e08626966ae164f1dad
Fernando Hermes <fernando.hermes@etermax.com>
2020-10-07 06:25:13 -0300

    RecorderHelper

---
+++ Assets/RecorderHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.Unity.PandoBehaviours;
+
+public class RecorderHelper : AutoLoadMonoBehaviour
+{
+
+    protected override void Load()
+    {
+        Cursor.visible = false;
+    }
+}
